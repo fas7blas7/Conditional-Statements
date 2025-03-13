c27b4f3652afaa6fe1f7b7190b050f8ecfb39369
Martin Kirov <fas7blas7@gmail.com>
2025-03-13 21:38:25 +0200

    Adding FreezingWeather task

---
+++ 1-FreezingWeather/1-FreezingWeather.cs
@@ -0,0 +1,14 @@
+﻿namespace _1_FreezingWeather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = int.Parse(Console.ReadLine());
+            if (temperature <= 0)
+            {
+                Console.WriteLine("Freezing weather!");
+            }        
+        }
+    }
+}
