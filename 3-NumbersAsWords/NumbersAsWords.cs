2e6ee231939574747fb2a11491e15a353896a1e8
Martin Kirov <fas7blas7@gmail.com>
2025-03-13 21:40:47 +0200

    Adding NumbersAsWords task

---
+++ 3-NumbersAsWords/NumbersAsWords.cs
@@ -0,0 +1,51 @@
+﻿namespace _3_NumbersAsWords
+{
+    internal class NumbersAsWords
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 1 && number < 10)
+            {
+                Console.WriteLine("one");
+            }
+            else if (number == 2 && number < 10)
+            {
+                Console.WriteLine("two");
+            }
+            else if (number == 3 && number < 10)
+            {
+                Console.WriteLine("three");
+            }
+            else if (number == 4 && number < 10)
+            {
+                Console.WriteLine("four");
+            }
+            else if (number == 5 && number < 10)
+            {
+                Console.WriteLine("five");
+            }
+            else if (number == 6 && number < 10)
+            {
+                Console.WriteLine("six");
+            }
+            else if (number == 7 && number < 10)
+            {
+                Console.WriteLine("seven");
+            }
+            else if (number == 8 && number < 10)
+            {
+                Console.WriteLine("eight");
+            }
+            else if (number == 9 && number < 10)
+            {
+                Console.WriteLine("nine");
+            }
+            else
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+    }
+}
