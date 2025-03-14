12dcd5a439e4951eb65002fa240c9f028ab39569
Martin Kirov <fas7blas7@gmail.com>
2025-03-14 20:13:19 +0200

    Adding SpeedInfo task

---
+++ 7-SpeedInfo/SpeedInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace _7_SpeedInfo
+{
+    internal class SpeedInfo
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+
+            if (speed <= 30)
+            {
+                Console.WriteLine("Slow");
+            }
+            else if (speed > 30)
+            {
+                Console.WriteLine("Fast");
+            }
+        }
+    }
+}
