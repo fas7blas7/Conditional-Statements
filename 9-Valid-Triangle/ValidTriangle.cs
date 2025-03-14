0546e96c847a0ff28e89eeefa1e2bca5f6def2d9
Martin Kirov <fas7blas7@gmail.com>
2025-03-14 20:14:20 +0200

    Adding ValidTriangle task

---
+++ 9-Valid-Triangle/ValidTriangle.cs
@@ -0,0 +1,21 @@
+﻿namespace _9_Valid_Triangle
+{
+    internal class ValidTriangle
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+            int sideC = int.Parse(Console.ReadLine());
+
+            if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB)
+            {
+                Console.WriteLine("Valid Triangle");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Triangle");
+            }
+        }
+    }
+}
