9b11c7d62f1e2da3d28efc1b3b5a1036bdaaba79
Martin Kirov <fas7blas7@gmail.com>
2025-03-14 20:13:44 +0200

    Adding TicketPrice task

---
+++ 8-TicketPrice/TicketPrice.cs
@@ -0,0 +1,25 @@
+﻿namespace _8_TicketPrice
+{
+    internal class TicketPrice
+    {
+        static void Main(string[] args)
+        {
+            string ticketType = Console.ReadLine();
+            double studentTicketPrice = 1.00;
+            double regularTicketPrice = 1.60;
+
+            if(ticketType == "student")
+            {
+                Console.WriteLine($"${studentTicketPrice:F2}");
+            }
+            else if (ticketType == "regular")
+            {
+                Console.WriteLine($"${regularTicketPrice:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid ticket type!");
+            }
+        }
+    }
+}
