dd54eb7027be2a18edaaa62777419d7182d51295
Martin Kirov <fas7blas7@gmail.com>
2025-03-14 20:09:22 +0200

    Adding BoilingWater task

---
+++ 6-BoilingWater/BoilingWater.cs
@@ -0,0 +1,19 @@
+﻿namespace _6_BoilingWater
+{
+    internal class BoilingWater
+    {
+        static void Main(string[] args)
+        {
+            int temperature = int.Parse(Console.ReadLine());
+
+            if (temperature > 100)
+            {
+                Console.WriteLine("The water is boiling");
+            }
+            else
+            {
+                Console.WriteLine("The water is not hot enough");
+            }
+        }
+    }
+}
