374bdb437e7132f96b7b7e9335ce43bd9093dd28
Martin Kirov <fas7blas7@gmail.com>
2025-03-14 20:08:53 +0200

    Adding GuessThePassword task

---
+++ 5-GuessThePassword/GuessThePassword.cs
@@ -0,0 +1,19 @@
+﻿namespace _5_GuessThePassword
+{
+    internal class GuessThePassword
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            if (password == "s3cr3t!")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
